7aff2161f332a77799f4eda2415998390589bb7b
Leo Xu <leo.xu.64@gmail.com>
2021-03-12 12:43:18 -0500

    removed pop

--- src/MapChart.js
+++ src/MapChart.js
@@ -9,16 +9,6 @@ import {
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-const rounded = num => {
-  if (num > 1000000000) {
-    return Math.round(num / 100000000) / 10 + "Bn";
-  } else if (num > 1000000) {
-    return Math.round(num / 100000) / 10 + "M";
-  } else {
-    return Math.round(num / 100) / 10 + "K";
-  }
-};
-
 const MapChart = ({ setTooltipContent, setSelected }) => {
   const [country, setCountry] = useState("")
   return (
@@ -40,7 +30,7 @@ const MapChart = ({ setTooltipContent, setSelected }) => {
                   geography={geo}
                   onMouseEnter={() => {
                     const { NAME, POP_EST } = geo.properties;
-                    setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+                    setTooltipContent(`${NAME}`);
                   }}
                   onMouseLeave={() => {
                     setTooltipContent("");
